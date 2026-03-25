3d93d51a00801a6db6c07d14c04f7148f0f27578
nathanmontalvoGM <nathanmontalvo1222@gmail.com>
2026-03-24 20:53:21 -0400

    Quartz sync: Mar 24, 2026, 8:53 PM

--- quartz.config.ts
+++ quartz.config.ts
@@ -17,7 +17,7 @@ const config: QuartzConfig = {
     },
     locale: "en-US",
     baseUrl: "quartz.jzhao.xyz",
-    ignorePatterns: ["private", "templates", ".obsidian", "TTRPG Design",],
+    ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "modified",
     theme: {
       fontOrigin: "googleFonts",
