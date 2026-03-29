ae36ba2e7406d79d9fb04ad8bc33d985f83a1363
nathanmontalvoGM <nathanmontalvo1222@gmail.com>
2026-03-29 09:53:33 -0400

    Quartz sync: Mar 29, 2026, 9:53 AM

--- quartz.config.ts
+++ quartz.config.ts
@@ -23,8 +23,8 @@ const config: QuartzConfig = {
       fontOrigin: "googleFonts",
       cdnCaching: true,
       typography: {
-        header: "Schibsted Grotesk",
-        body: "Source Sans Pro",
+        header: "Cinzel",
+        body: "Palatino Linotype",
         code: "IBM Plex Mono",
       },
       colors: {
