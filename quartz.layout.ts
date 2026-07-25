fd6796d7b38295646ca4a3b4289dab4c884793a4
nathanmontalvoGM <nathanmontalvo1222@gmail.com>
2026-07-25 09:33:45 -0400

    Quartz sync: Jul 25, 2026, 9:33 AM

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -15,6 +15,7 @@ export const sharedPageComponents: SharedLayout = {
     },
   }),
 }
+
 // components for pages that display a single page (e.g. a single note)
 export const defaultContentPageLayout: PageLayout = {
   beforeBody: [
@@ -29,12 +30,6 @@ export const defaultContentPageLayout: PageLayout = {
   left: [
     Component.PageTitle(),
     Component.MobileOnly(Component.Spacer()),
-    Component.Explorer({
-      filterFn: (node) => {
-    const hiddenFolders = ["Templates", "Archive", "Private"];
-    return !hiddenFolders.includes(node.name);
-  },
-}),
   ],
   right: [
     Component.Graph(),
@@ -43,20 +38,12 @@ export const defaultContentPageLayout: PageLayout = {
   ],
 }
 
-// components for pages that display lists of pages  (e.g. tags or folders)
+// components for pages that display lists of pages (e.g. tags or folders)
 export const defaultListPageLayout: PageLayout = {
   beforeBody: [Component.Breadcrumbs(), Component.ArticleTitle(), Component.ContentMeta()],
   left: [
     Component.PageTitle(),
     Component.MobileOnly(Component.Spacer()),
-    Component.Explorer({
-      filterFn: (node) => {
-    const hiddenFolders = ["Templates", "Archive", "Private","00 Explaining the System", "00 System Core", "OLD", "old", "UNFINISHED", "TODO", "00 Unfinished", 
-      "v1", "v2", "v3", "v4", "v5", "v6", "v7", "v8", "v9", "v10", "V1", "V2", "V3", "V4", "V5", "V6", "V7", "V8", "V9", "V10", "TTRPG Design", "Rejected Ideas", "PAD TODO" ];
-    return !hiddenFolders.includes(node.name);
-  },
-}),
-
   ],
   right: [],
 }
